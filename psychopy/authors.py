ccf2d3d4c515ace10d39b65fe5f55d18dcb33990
Todd <todd.e.parsons@googlemail.com>
2021-12-22 12:04:09 +0000

    NF: Author class to handle attributions

---
+++ psychopy/authors.py
@@ -0,0 +1,52 @@
+import inspect
+
+
+class Author:
+    _refs = {}
+
+    def __init__(self, name, ref=None):
+        # Store name
+        self.name = name
+        if ref is not None:
+            # Store thing the author is attributed to, if given
+            if self.name not in Author._refs:
+                Author._refs[self.name] = []
+            Author._refs[self.name].append(ref)
+
+    def __repr__(self):
+        return (f"<"
+                f"Author: {self.name}, {len(self.all)} contribution(s): "
+                f"{len(self.classes)} class(es), {len(self.functions)} function(s)"
+                f">")
+
+    @property
+    def all(self):
+        """
+        List of all contributions credited to this author in modules which have been imported
+        """
+        return Author._refs[self.name]
+
+    @property
+    def classes(self):
+        """
+        List of classes credited to this author in modules which have been imported
+        """
+        return [ref for ref in Author._refs[self.name] if inspect.isclass(ref)]
+
+    @property
+    def functions(self):
+        """
+        List of functions credited to this author in modules which have been imported
+        """
+        return [ref for ref in Author._refs[self.name] if inspect.isfunction(ref) or inspect.ismethod(ref)]
+
+
+def getAll():
+    """
+    List all authors credited in modules which have been imported
+    """
+    # Get all authors currently referenced
+    auths = [Author(name) for name in Author._refs]
+    # Sort by number of contribs
+    auths.sort(key=lambda obj: len(obj.all), reverse=True)
+    return auths
