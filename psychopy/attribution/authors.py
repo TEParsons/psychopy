ec77a424eeea5bdbf8ca6f5059ff6b32c85f6e67
Todd Parsons <todd.e.parsons@googlemail.com>
2022-07-01 16:43:41 +0100

    ENH: Add some authors

---
+++ psychopy/attribution/authors.py
@@ -0,0 +1,37 @@
+from ._base import Author
+
+
+authors = [
+    # Current OST staff
+    Author("Peirce", "Jon",
+           email="jon@opensciencetools.org", github="peircej", orcid="0000-0002-9504-4342"),
+    Author("Parsons", "Todd", middlenames="Ethan",
+           email="todd@opensciencetools.org", github="TEParsons", orcid="0000-0002-6192-8414",
+           other={"website": "toddparsons.co.uk"}),
+    Author("Cutone", "Matthew", middlenames="D",
+           email="matthew@opensciencetools.org", github="mdcutone"),
+    Author("Simpson", "Sol",
+           email="sol@opensciencetools.org", github="isolver"),
+    Author("Hirst", "Rebecca",
+           email="becca@opensciencetools.org", github="RHirst"),
+    Author("", "Kimberley",
+           email="kim@opensciencetools.org"),
+    Author("Mah", "Sue", middlenames="Lynn",
+           email="suelynn@opensciencetools.org"),
+    Author("Pitoit", "Alain",
+           email="alain@opensciencetools.org", github="apitoit"),
+    Author("", "Nikita",
+           email="nikita@opensciencetools.org"),
+    # Former OST staff
+    Author("Bridges", "David"),
+    Author("Sharman", "Rebecca"),
+    Author("Pronk", "Thomas",
+           github="tpronk"),
+    Author("Bakagiannis", "Sotiri",
+           github="thewhodidthis"),
+    # Other contributors
+    Author("McGaskill", "Michael"),
+    Author("Gray", "Jeremy"),
+    Author("Haffey", "Anthony"),
+    Author("Haenel", "Valentin")
+]
