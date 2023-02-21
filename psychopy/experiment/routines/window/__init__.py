491ba015e0f7257f69356423efa3431afed01be3
Todd Parsons <todd.e.parsons@googlemail.com>
2023-02-21 11:37:34 +0000

    ENH: Order params and make sure behaviour is as before

--- psychopy/experiment/routines/window/__init__.py
+++ psychopy/experiment/routines/window/__init__.py
@@ -15,7 +15,7 @@ class WindowRoutine(BaseStandaloneRoutine):
             self, exp, name='',
             fullScr=True, winSize=(1024, 768), units='height',
             screen=1, monitor='testMonitor', showMouse=False,
-            color='white', colorSpace='rgb',
+            color='$[0, 0, 0]', colorSpace='rgb',
             backgroundImg="", backgroundFit="none",
             blendMode='avg', winBackend='pyglet',
     ):
@@ -23,42 +23,96 @@ class WindowRoutine(BaseStandaloneRoutine):
         # Delete stop params
         del self.params['stopVal']
         del self.params['stopType']
-        self.params['fullScr'] = Param(
-            fullScr, valType='bool', inputType="bool", allowedTypes=[],
-            hint=_translate("Run the experiment full-screen (recommended)"),
-            label=_translate("Full-screen window"), categ="Basic")
+
+        # --- Basic ---
+        self.order += [
+            "monitor",
+            "winBackend",
+            "blendMode",
+            "showMouse",
+        ]
+        self.params['monitor'] = Param(
+            monitor, valType='str', inputType="single", categ="Basic",
+            hint=_translate("Name of the monitor (from Monitor Center). Right"
+                            "-click to go there, then copy & paste a monitor "
+                            "name here."),
+            label=_translate("Monitor")
+        )
         self.params['winBackend'] = Param(
             winBackend, valType='str', inputType="choice", categ="Basic",
             allowedVals=plugins.getWindowBackends(),
             hint=_translate("What Python package should be used behind the scenes for drawing to the window?"),
             label=_translate("Window backend")
         )
-        self.params['winSize'] = Param(
-            winSize, valType='list', inputType="single", allowedTypes=[],
-            hint=_translate("Size of window (if not fullscreen)"),
-            label=_translate("Window size (pixels)"), categ="Layout")
+        self.params['blendMode'] = Param(
+            blendMode, valType='str', inputType="choice", categ="Basic",
+            allowedVals=['add', 'avg', 'nofbo'],
+            allowedLabels=['add', 'average', 'average (no FBO)'],
+            hint=_translate("Should new stimuli be added or averaged with "
+                            "the stimuli that have been drawn already"),
+            label=_translate("blendMode")
+        )
+        self.params['showMouse'] = Param(
+            showMouse, valType='bool', inputType="bool", categ="Basic",
+            hint=_translate("Should the mouse be visible on screen? Only applicable for fullscreen experiments."),
+            label=_translate("Show mouse")
+        )
+
+        # --- Layout ---
+        self.order += [
+            "screen",
+            "fullScr",
+            "winSize",
+            "units",
+        ]
         self.params["screen"] = Param(
-            screen, valType='num', inputType="spin", allowedTypes=[],
+            screen, valType='num', inputType="single", categ="Layout",
             hint=_translate("Which physical screen to run on (1 or 2)"),
-            label=_translate("Screen"), categ="Basic")
-        self.params['monitor'] = Param(
-            monitor, valType='str', inputType="single", allowedTypes=[],
-            hint=_translate("Name of the monitor (from Monitor Center). Right"
-                            "-click to go there, then copy & paste a monitor "
-                            "name here."),
-            label=_translate("Monitor"), categ="Basic")
+            label=_translate("Screen")
+        )
+        self.params['fullScr'] = Param(
+            fullScr, valType='bool', inputType="bool", categ="Layout",
+            hint=_translate("Run the experiment full-screen (recommended)"),
+            label=_translate("Full-screen window")
+        )
+        self.params['winSize'] = Param(
+            winSize, valType='list', inputType="single", categ="Layout",
+            hint=_translate("Size of window (if not fullscreen)"),
+            label=_translate("Window size (pixels)")
+        )
+        self.params['units'] = Param(
+            units, valType='str', inputType="choice", categ="Layout",
+            allowedVals=['use prefs', 'deg', 'pix', 'cm', 'norm', 'height',
+                         'degFlatPos', 'degFlat'],
+            hint=_translate("Units to use for window/stimulus coordinates "
+                            "(e.g. cm, pix, deg)"),
+            label=_translate("Units")
+        )
+
+        self.depends.append(
+            {"dependsOn": 'fullScr',  # must be param name
+             "condition": "==True",  # val to check for
+             "param": 'winSize',  # param property to alter
+             "true": "hide",  # what to do with param if condition is True
+             "false": "show",  # permitted: hide, show, enable, disable
+             }
+        )
+
+        # --- Appearance ---
         self.params['color'] = Param(
-            color, valType='color', inputType="color", allowedTypes=[],
+            color, valType='color', inputType="color", categ="Appearance",
             hint=_translate("Color of the screen (e.g. black, $[1.0,1.0,1.0],"
                             " $variable. Right-click to bring up a "
                             "color-picker.)"),
-            label=_translate("color"), categ="Appearance")
+            label=_translate("color")
+        )
         self.params['colorSpace'] = Param(
-            colorSpace, valType='str', inputType="choice",
+            colorSpace, valType='str', inputType="choice", categ="Appearance",
+            allowedVals=['rgb', 'dkl', 'lms', 'hsv', 'hex'],
             hint=_translate("Needed if color is defined numerically (see "
                             "PsychoPy documentation on color spaces)"),
-            allowedVals=['rgb', 'dkl', 'lms', 'hsv', 'hex'],
-            label=_translate("colorSpace"), categ="Appearance")
+            label=_translate("colorSpace")
+        )
         self.params['backgroundImg'] = Param(
             backgroundImg, valType="str", inputType="file", categ="Appearance",
             hint=_translate("Image file to use as a background (leave blank for no image)"),
@@ -70,24 +124,6 @@ class WindowRoutine(BaseStandaloneRoutine):
             hint=_translate("How should the background image scale to fit the window size?"),
             label=_translate("Background fit")
         )
-        self.params['units'] = Param(
-            units, valType='str', inputType="choice", allowedTypes=[],
-            allowedVals=['use prefs', 'deg', 'pix', 'cm', 'norm', 'height',
-                         'degFlatPos', 'degFlat'],
-            hint=_translate("Units to use for window/stimulus coordinates "
-                            "(e.g. cm, pix, deg)"),
-            label=_translate("Units"), categ="Layout")
-        self.params['blendMode'] = Param(
-            blendMode, valType='str', inputType="choice",
-            allowedVals=['add', 'avg', 'nofbo'],
-            allowedLabels=['add', 'average', 'average (no FBO)'],
-            hint=_translate("Should new stimuli be added or averaged with "
-                            "the stimuli that have been drawn already"),
-            label=_translate("blendMode"), categ="Basic")
-        self.params['showMouse'] = Param(
-            showMouse, valType='bool', inputType="bool", allowedTypes=[],
-            hint=_translate("Should the mouse be visible on screen? Only applicable for fullscreen experiments."),
-            label=_translate("Show mouse"), categ="Basic")
         # self.depends.append(
         #     {"dependsOn": 'Full-screen window',  # must be param name
         #      "condition": "==True",  # val to check for
@@ -130,7 +166,7 @@ class WindowRoutine(BaseStandaloneRoutine):
             # computer has 1 as first screen
             params['screen'].val = requestedScreenNumber - 1
         # Integer screen num
-        params['screen'].val = int(params['screen'].val)
+        params['screen'] = int(params['screen'].val)
         # Blend mode
         params['useFBO'] = True
         if params['blendMode'].val in ("nofbo",):
@@ -139,14 +175,18 @@ class WindowRoutine(BaseStandaloneRoutine):
         # Units from prefs
         if params['units'] == 'use prefs':
             params['units'] = None
+        # Only include size spec if window is not full screen
+        params['sizeStr'] = "size=%(winSize)s, " % params
+        if params['fullScr']:
+            params['sizeStr'] = ""
 
         # --- Write creation code ---
         code = (
             "# --- Create window '%(name)s' ---\n"
             "%(name)s = visual.Window(\n"
-            "    screen=%(screen)s, winType=%(winBackend)s, \n"
+            "    screen=%(screen)d, winType=%(winBackend)s,\n"
             "    fullscr=%(fullScr)s, monitor=%(monitor)s,\n"
-            "    size=%(winSize)s, units=%(units)s,\n"
+            "    %(sizeStr)sunits=%(units)s,\n"
             "    color=%(color)s, colorSpace=%(colorSpace)s,\n"
             "    backgroundImage=%(backgroundImg)s, backgroundFit=%(backgroundFit)s,\n"
             "    blendMode=%(blendMode)s, useFBO=%(useFBO)s, allowStencil=%(allowStencil)s\n"
