4d26d4a623edca87b564a652251533f128a0b1cd
Todd Parsons <todd.e.parsons@googlemail.com>
2023-03-23 16:36:39 +0000

    NF: Some basic JS-isms for convenience

---
+++ psychopy/javascriptisms.py
@@ -0,0 +1,10 @@
+"""
+Common JS phrases which have direct equivalencies in Python, importing at experiment start means designers can
+use these in Code components and stimulus params without worry.
+"""
+
+Boolean = bool
+Number = float
+String = str
+Object = object
+Infinity = float("inf")
