487dfa7665327eb24da1607674ada8e452fea6a5
Todd Parsons <todd.e.parsons@googlemail.com>
2023-08-10 12:54:13 +0100

    ENH: Apply base class to BoolCtrl

--- psychopy/app/builder/paramCtrls/bool.py
+++ psychopy/app/builder/paramCtrls/bool.py
@@ -1,3 +1,35 @@
 import wx
+from .base import BaseParamCtrl
 
-BoolCtrl = wx.CheckBox
+
+class BoolCtrl(BaseParamCtrl):
+    tag = "bool"
+
+    def __init__(self, parent, param, fieldName=""):
+        # initialise
+        BaseParamCtrl.__init__(self, parent, param, fieldName="")
+        # add bool ctrl
+        self.ctrl = wx.CheckBox(self)
+        self.sizer.Add(self.ctrl, proportion=1, border=3, flag=wx.EXPAND | wx.ALL)
+
+    def getValue(self):
+        """
+        Get the value of this ctrl.
+
+        Returns
+        -------
+        object
+            Value of this ctrl.
+        """
+        self.ctrl.GetValue()
+
+    def setValue(self, value):
+        """
+        Set the value of this ctrl.
+
+        Parameters
+        ----------
+        value
+            Value of this ctrl.
+        """
+        self.ctrl.SetValue(value)
