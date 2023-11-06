d1b26ccdcfdee1bacd87917f6a9262ba634577df
Todd Parsons <todd.e.parsons@googlemail.com>
2023-11-06 19:14:51 +0000

    RF: Move device adding to its own dialog

--- psychopy/app/builder/dialogs/dlgDeviceManager.py
+++ psychopy/app/builder/dialogs/dlgDeviceManager.py
@@ -7,24 +7,24 @@ from psychopy.localization import _translate
 from psychopy.hardware import DeviceManager
 
 
-class DeviceNode(wx.Panel):
-    def __init__(self, parent, dlg, device, action):
+class DeviceListNode(wx.Panel):
+    """
+    Node representing the static information of an available device
+    """
+    def __init__(self, parent, dlg, device):
         wx.Panel.__init__(self, parent, style=wx.BORDER_SIMPLE)
         self.parent = parent
         self.dlg = dlg
-        self.device = device
         self.SetBackgroundColour("white")
-        # pop name and class from dict
-        device = device.copy()
-        deviceName = device.pop('deviceName')
-        deviceClass = device.pop('deviceClass')
-        deviceClass = DeviceManager._resolveAlias(deviceClass)
+        # store device info
+        self.device = device
         # setup sizers
         self.border = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.border)
         self.sections = wx.BoxSizer(wx.HORIZONTAL)
         self.border.Add(self.sections, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
         # get device class
+        deviceClass = DeviceManager._resolveAlias(self.device['deviceClass'])
         cls = DeviceManager._resolveClassString(deviceClass)
         # make icon
         if cls.components:
@@ -32,36 +32,43 @@ class DeviceNode(wx.Panel):
             icon = icons.ComponentIcon(iconCls).bitmap
         else:
             icon = wx.Bitmap()
+        # add icon
         self.icon = wx.StaticBitmap(self, bitmap=icon)
         self.sections.Add(self.icon, border=6, flag=wx.ALIGN_TOP | wx.ALL)
-        # create main sizer
+
+        # add main content
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.sections.Add(self.sizer, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
+        # add summary sizer
+        self.summarySizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.summarySizer, border=6, flag=wx.EXPAND)
         # add title
-        if action == "add":
-            self.deviceName = wx.StaticText(self, label=deviceName)
-        else:
-            self.deviceName = wx.TextCtrl(self, value=deviceName)
+        self.deviceName = wx.StaticText(self, label=device['deviceName'])
         self.deviceName.SetFont(fonts.AppFont(12).obj)
-        self.sizer.Add(self.deviceName, flag=wx.EXPAND | wx.ALL)
+        self.summarySizer.Add(self.deviceName, flag=wx.EXPAND | wx.ALL)
         # add class name
         self.deviceClass = wx.StaticText(self, label=deviceClass)
         self.deviceClass.SetFont(fonts.CodeFont(10).obj)
         self.deviceClass.SetForegroundColour("#acacb0")
-        self.sizer.Add(self.deviceClass)
+        self.summarySizer.Add(self.deviceClass)
+        # add details sizer
+        self.detailsSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.detailsSizer, border=6, flag=wx.EXPAND)
         # add separator
-        self.sep = wx.StaticLine(self, style=wx.LI_VERTICAL)
-        self.sizer.Add(self.sep, border=6, flag=wx.EXPAND | wx.ALL)
+        self.detailsSizer.AddSpacer(12)
         # add details
         detailsStrList = []
         for key, val in device.items():
+            if key in ("deviceName", "deviceClass"):
+                continue
             detailsStrList.append(
                 f"{key}: {val}"
             )
         detailsStr = "\n".join(detailsStrList)
         self.details = wx.StaticText(self, label=detailsStr)
         self.details.SetFont(fonts.CodeFont(10).obj)
-        self.sizer.Add(self.details)
+        self.detailsSizer.Add(self.details)
+
         # add buttons sizer
         self.btnsSizer = wx.BoxSizer(wx.VERTICAL)
         self.sections.Add(self.btnsSizer, border=6, flag=wx.ALIGN_TOP | wx.ALL)
@@ -69,24 +76,39 @@ class DeviceNode(wx.Panel):
         self.toggleView = wx.ToggleButton(self, style=wx.BU_EXACTFIT)
         self.toggleView.Bind(wx.EVT_TOGGLEBUTTON, self.onToggleDetails)
         self.btnsSizer.Add(self.toggleView, border=6, flag=wx.EXPAND | wx.BOTTOM)
-        # add add button
-        self.addBtn = wx.Button(self, style=wx.BU_EXACTFIT)
-        self.addBtn.SetBitmap(
-            icons.ButtonIcon("add", size=16).bitmap
-        )
-        self.addBtn.Bind(wx.EVT_BUTTON, self.onAdd)
-        self.btnsSizer.Add(self.addBtn, border=6, flag=wx.EXPAND | wx.BOTTOM)
-        self.addBtn.Show(action == "add")
-        # add remove button
-        self.removeBtn = wx.Button(self, style=wx.BU_EXACTFIT)
-        self.removeBtn.SetBitmap(
-            icons.ButtonIcon("delete", size=16).bitmap
-        )
-        self.removeBtn.Bind(wx.EVT_BUTTON, self.onRemove)
-        self.btnsSizer.Add(self.removeBtn, border=6, flag=wx.EXPAND | wx.BOTTOM)
-        self.removeBtn.Show(action == "remove")
 
+        # setup selection behaviour
+        self.setupSelectionBehaviour()
+        # set initial toggle to be off
         self.onToggleDetails(False)
+
+    def getInfo(self):
+        # get name and class
+        info = {
+            'deviceName': self.deviceName.GetLabel(),
+            'deviceClass': self.deviceClass.GetLabel()
+        }
+        # get the rest
+        detailsStr = self.details.GetLabel()
+        for line in detailsStr.split("\n"):
+            if ": " not in line:
+                continue
+            key, val = line.split(": ", maxsplit=1)
+            info[key] = val
+
+        return info
+
+    def setupSelectionBehaviour(self):
+        """
+        Setup selection behaviour
+        """
+        self.Bind(wx.EVT_LEFT_DOWN, self.onSelect)
+        for child in self.GetChildren():
+            if isinstance(child, (wx.StaticText, utils.WrappedStaticText, wx.StaticBitmap)):
+                child.Bind(wx.EVT_LEFT_DOWN, self.onSelect)
+
+    def onSelect(self, evt=None):
+        self.parent.setSelection(self)
 
     def onToggleDetails(self, evt=None):
         # if called with a boolean, use it directly, otherwise get bool from event
@@ -101,41 +123,15 @@ class DeviceNode(wx.Panel):
             lbl = "⯇"
         self.toggleView.SetLabel(lbl)
         # show/hide details based on value
-        self.details.Show(val)
-        self.sep.Show(val)
+        for child in self.detailsSizer.GetChildren():
+            child.Show(val)
         # layout
         self.parent.Layout()
         self.parent.SetupScrolling(scroll_x=False, scroll_y=True)
 
-    def getInfo(self):
-        info = {}
-        # get name
-        if isinstance(self.deviceName, wx.StaticText):
-            info['deviceName'] = self.deviceName.GetLabel()
-        else:
-            info['deviceName'] = self.deviceName.GetValue()
-        # get class
-        info['deviceClass'] = self.deviceClass.GetLabel()
-        # get the rest
-        detailsStr = self.details.GetLabel()
-        for line in detailsStr.split("\n"):
-            if ": " not in line:
-                continue
-            key, val = line.split(": ", maxsplit=1)
-            info[key] = val
 
-        return info
-
-    def onAdd(self, evt=None):
-        self.dlg.addedCtrl.addNode(self.device, action="remove")
-
-    def onRemove(self, evt=None):
-        self.Destroy()
-        self.parent.Layout()
-
-
-class DeviceManagerListCtrl(scrolledpanel.ScrolledPanel):
-    def __init__(self, parent, title, hint=""):
+class DeviceListCtrl(scrolledpanel.ScrolledPanel):
+    def __init__(self, parent):
         scrolledpanel.ScrolledPanel.__init__(self, parent)
         self.dlg = parent
         self.SetBackgroundColour("white")
@@ -144,81 +140,86 @@ class DeviceManagerListCtrl(scrolledpanel.ScrolledPanel):
         self.SetSizer(self.border)
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.border.Add(self.sizer, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
-        # add title
-        self.title = utils.WrappedStaticText(self, label=title)
-        self.title.SetBackgroundColour("white")
-        self.title.SetFont(fonts.AppFont(14).obj)
-        self.sizer.Add(self.title, border=6, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP)
-        # add hint
-        self.hint = utils.WrappedStaticText(self, label=hint)
-        self.hint.SetBackgroundColour("white")
-        self.sizer.Add(self.hint, border=6, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM)
+        # setup selection behaviour
+        self.selected = None
+        self.Bind(wx.EVT_LEFT_DOWN, self.onDeselect)
 
         self.Layout()
 
-    def addNode(self, device, action):
-        node = DeviceNode(self, self.dlg, device, action)
+    def addNode(self, device):
+        node = DeviceListNode(parent=self, dlg=self.dlg, device=device)
         self.sizer.Add(node, border=6, flag=wx.EXPAND | wx.ALL)
 
         self.dlg.Layout()
 
+    def onDeselect(self, evt=None):
+        self.setSelection(None)
+
+    def setSelection(self, node):
+        # unstyle previous selection
+        if isinstance(self.selected, DeviceListNode):
+            self.selected.SetBackgroundColour(self.GetBackgroundColour())
+            self.selected.Update()
+            self.selected.Refresh()
+        # select new node
+        self.selected = node
+        # style new selection
+        if isinstance(self.selected, DeviceListNode):
+            self.selected.SetBackgroundColour("#f2f2f2")
+            self.selected.Update()
+            self.selected.Refresh()
+
+    def getSelection(self):
+        return self.selected
+
     def clearNodes(self):
         for node in self.GetChildren():
-            if isinstance(node, DeviceNode):
+            if isinstance(node, DeviceListNode):
                 node.Destroy()
 
 
-class DeviceManagerDlg(wx.Dialog):
+class AddDeviceDlg(wx.Dialog):
     def __init__(self, parent=None):
         wx.Dialog.__init__(
             self, parent,
-            title=_translate("Device manager"),
-            size=(1080, 720),
+            title=_translate("Add device..."),
+            size=(540, 720),
             style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
         )
         # setup sizers
         self.border = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.border)
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.border.Add(self.sizer, border=12, proportion=1, flag=wx.EXPAND | wx.ALL)
-        # add available devices ctrl
-        self.availableCtrl = DeviceManagerListCtrl(
-            self,
-            title=_translate("Available"),
-            hint=_translate(
+        # add instructions
+        self.instr = utils.WrappedStaticText(
+            self, label=_translate(
                 "These devices have been detected by PsychoPy as being available, but have not necessarily been "
-                "configured in this experiment's Device Manager."
+                "configured in this experiment's device manager. Select a device and click 'OK' to add it to the "
+                "device manager."
             )
         )
-        self.sizer.Add(self.availableCtrl, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
+        self.sizer.Add(self.instr, border=6, flag=wx.EXPAND | wx.ALL)
         # add available devices ctrl
-        self.addedCtrl = DeviceManagerListCtrl(
-            self,
-            title=_translate("Added"),
-            hint=_translate(
-                "These devices have been configured in this experiment's Device Manager. You can refer to these devices "
-                "by name in the 'device' field of Components your experiment."
-            )
-        )
-        self.sizer.Add(self.addedCtrl, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
-        # add buttons
-        btns = self.CreateStdDialogButtonSizer(flags=wx.CANCEL | wx.OK)
-        self.Bind(wx.EVT_BUTTON, self.onOK, id=wx.ID_OK)
-        self.border.Add(btns, border=12, flag=wx.EXPAND | wx.ALL)
+        self.ctrl = DeviceListCtrl(self)
+        self.sizer.Add(self.ctrl, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
         # scan for available devices and populate
         self.available = {}
         self.rescan(repop=True)
+        # add buttons
+        btns = self.CreateStdDialogButtonSizer(flags=wx.CANCEL | wx.OK)
+        self.border.Add(btns, border=12, flag=wx.EXPAND | wx.ALL)
 
     def populate(self, evt=None):
         # clear any extant nodes
-        self.availableCtrl.clearNodes()
+        self.ctrl.clearNodes()
         # make nodes
         for devs in self.available.values():
             for device in devs:
-                self.availableCtrl.addNode(device, action="add")
+                self.ctrl.addNode(device)
 
         self.Layout()
-        self.availableCtrl.SetupScrolling(scroll_x=False, scroll_y=True)
+        self.ctrl.SetupScrolling(scroll_x=False, scroll_y=True)
 
     def rescan(self, evt=None, repop=True):
         """
@@ -252,11 +253,59 @@ class DeviceManagerDlg(wx.Dialog):
 
         return self.available
 
+
+class DeviceManagerDlg(wx.Dialog):
+    def __init__(self, parent=None):
+        wx.Dialog.__init__(
+            self, parent,
+            title=_translate("Device manager"),
+            size=(1080, 720),
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER
+        )
+        # setup sizers
+        self.border = wx.BoxSizer(wx.VERTICAL)
+        self.SetSizer(self.border)
+        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.border.Add(self.sizer, border=12, proportion=1, flag=wx.EXPAND | wx.ALL)
+        self.leftSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.leftSizer, border=6, flag=wx.EXPAND | wx.ALL)
+        self.rightSizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.rightSizer, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
+
+        # add instructions
+        self.instr = utils.WrappedStaticText(
+            self, label=_translate(
+                "These devices have been configured in this experiment's Device Manager. You can refer to them "
+                "by name in the 'device' field of Components your experiment."
+            )
+        )
+        self.leftSizer.Add(self.instr, border=6, flag=wx.EXPAND | wx.ALL)
+        # add available devices ctrl
+        self.ctrl = DeviceListCtrl(self)
+        self.leftSizer.Add(self.ctrl, border=6, proportion=1, flag=wx.EXPAND | wx.ALL)
+        # add "add device" button
+        self.addBtn = wx.Button(self, label=_translate("Add device..."))
+        self.addBtn.Bind(wx.EVT_BUTTON, self.onAdd)
+        self.leftSizer.Add(self.addBtn, border=6,flag=wx.ALIGN_LEFT | wx.ALL)
+
+        # add buttons
+        btns = self.CreateStdDialogButtonSizer(flags=wx.CANCEL | wx.OK)
+        self.Bind(wx.EVT_BUTTON, self.onOK, id=wx.ID_OK)
+        self.border.Add(btns, border=12, flag=wx.EXPAND | wx.ALL)
+
+        self.Layout()
+
+    def onAdd(self, evt=None):
+        dlg = AddDeviceDlg(self)
+        if dlg.ShowModal() == wx.ID_OK:
+            if isinstance(dlg.ctrl.selected, DeviceListNode):
+                self.ctrl.addNode(dlg.ctrl.selected.getInfo())
+
     def onOK(self, evt=None):
         devices = []
         for node in self.addedCtrl.GetChildren():
             # skip labels and such
-            if not isinstance(node, DeviceNode):
+            if not isinstance(node, DeviceListNode):
                 continue
             # get node info
             devices.append(node.getInfo())
